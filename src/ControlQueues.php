ef01ed34a7cfe05b76498af6496da1e0475c3446
Carlos Izarra <55459636+CarlosIzarra09@users.noreply.github.com>
2023-01-31 15:14:44 -0500

    feat: Reformat controlQueue class

--- src/ControlQueues.php
+++ src/ControlQueues.php
@@ -1,7 +1,58 @@
 <?php
 
+Class ControlQueue {
+    protected $registry;
+    protected static $instance;
 
-Class ControlQueuesTickets {
+    protected function __construct(){
+        $this->registry = new SplQueue();
+    }
+
+    public static function getInstance()
+    {
+        if (!isset(self::$instance)) {
+        // late static binding
+            self::$instance = new self;
+            
+        }
+        return self::$instance;
+    }
+
+    public function addRegistryItem(string $item):void{
+        $this->registry->enqueue($item);
+    }
+
+    public function getRegistryQueue(): SplQueue{
+        return $this->registry;
+    }
+
+    public function popTopRegistryItem(): void{
+        $this->registry->dequeue();
+    }
+
+}
+
+
+Class ControlQueueTicket extends ControlQueue {
+
+}
+
+Class ControlQueueProblem extends ControlQueue {
+
+}
+
+Class ControlQueueChange extends ControlQueue{
+
+}
+
+
+
+
+
+
+
+
+/*Class ControlQueuesTickets {
 
     private $registry_tickets;
     private static $instance;
@@ -46,4 +97,4 @@ Class ControlQueuesTickets {
 
     
 
-}
+}*/
